8628da918e48b46b8298a54736ad1cff2db05c90
Andrew Swerlick <andrew.swerlick@hbdi.com>
2015-02-01 15:11:12 -0500

    day 3 notes

--- code/ThreadsLocks/WordCount/src/main/java/com/paulbutcher/WordCount.java
+++ code/ThreadsLocks/WordCount/src/main/java/com/paulbutcher/WordCount.java
@@ -1,9 +1,9 @@
 /***
  * Excerpted from "Seven Concurrency Models in Seven Weeks",
  * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
+ * Copyrights apply to this code. It may not be used to create training material,
  * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
+ * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/pb7con for more book information.
 ***/
 package com.paulbutcher;
@@ -12,12 +12,12 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class WordCount {
-  private static final HashMap<String, Integer> counts = 
+  private static final HashMap<String, Integer> counts =
     new HashMap<String, Integer>();
 
   public static void main(String[] args) throws Exception {
     long start = System.currentTimeMillis();
-    Iterable<Page> pages = new Pages(100000, "enwiki.xml");
+    Iterable<Page> pages = new Pages(100000, "/Users/andrewswerlick/Projects/play/book_club/concurrency/enwiki.xml");
     for(Page page: pages) {
       Iterable<String> words = new Words(page.getText());
       for (String word: words)
